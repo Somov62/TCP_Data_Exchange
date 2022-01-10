d8de17de707c6b1c39db6364720ce637d8c4dbf3
Somov <misha2003980@gmail.com>
2022-01-11 01:33:52 +0300

    Добавьте файлы проекта.

---
+++ TCP_Data_Exchange/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCP_Data_Exchange
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool IsServerActive;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Host_Click(object sender, RoutedEventArgs e)
+        {
+            IsServerActive = true;
+            ListenAsync();
+        }
+        private async Task ListenAsync()
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+            TcpListener server = new TcpListener(localAddr, port);
+            server.Start();
+            // Выполняем цикл только, если серверная часть «включена»
+            while (IsServerActive)
+            {
+                try
+                {
+                    // Асинхронное подключение клиента
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    NetworkStream stream = client.GetStream();
+                    // Обмен данными только, если серверная часть «включена».
+                    try
+                    {
+                        // Читаем данные
+                        if (stream.CanRead && IsServerActive)
+                        {
+                            byte[] myReadBuffer = new byte[1024];
+                            StringBuilder myCompleteMessage = new StringBuilder();
+                            int numberOfBytesRead = 0;
+                            do
+                            {
+                                numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                                myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(myReadBuffer, 0, numberOfBytesRead));
+                            }
+                            while (stream.DataAvailable);
+                            Console.WriteLine(myCompleteMessage);
+                            Byte[] responseData = Encoding.UTF8.GetBytes("УСПЕШНО!");
+                            stream.Write(responseData, 0, responseData.Length);
+                        }
+                    }
+                    finally
+                    {
+                        stream.Close();
+                        client.Close();
+                    }
+                }
+                catch
+                {
+                    server.Stop();
+                    break;
+                }
+            }
+            // Если серверная часть «выключена», обязательно останавливаем прослушивание порта.
+            // Иначе потом серверная часть не «включится».
+            if (!IsServerActive)
+            {
+                server.Stop();
+            }
+        }
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            // Инициализация
+            TcpClient client = new TcpClient("127.0.0.1", 8888);
+            Byte[] data = Encoding.UTF8.GetBytes("hello");
+            NetworkStream stream = client.GetStream();
+            try
+            {
+                // Отправка сообщения
+                stream.Write(data, 0, data.Length);
+                // Получение ответа
+                Byte[] readingData = new Byte[256];
+                String responseData = String.Empty;
+                StringBuilder completeMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+                do
+                {
+                    numberOfBytesRead = stream.Read(readingData, 0, readingData.Length);
+                    completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, numberOfBytesRead));
+                }
+                while (stream.DataAvailable);
+                responseData = completeMessage.ToString();
+                Console.WriteLine(responseData);
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+        }
+    }
+}
